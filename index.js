24e0978a7d8017da09b91a829b4afefd399526c3
NusratJahanGeek <nusrat.jahan.4338@gmail.com>
2023-06-10 02:06:44 +0600

    Read, Update, and Delete Operations Added for Users

--- index.js
+++ index.js
@@ -32,9 +32,65 @@ async function run() {
     const bookingCollection = client.db("campCraftopiaDB").collection("bookings");
     
     // Users Related APIs
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'User Already Exists'})
+      }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateRole = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateRole);
+      res.send(result);
+    });
+
+    app.patch('/users/instructor/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateRole = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateRole);
+      res.send(result);
+    });
+
+    app.patch('/users/user/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateRole = {
+        $set: {
+          role: 'user'
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateRole);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
